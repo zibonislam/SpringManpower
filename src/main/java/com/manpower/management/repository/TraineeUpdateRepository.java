76d530c078f05fa2d4be7165fb9a62d188eb0f24
zibonislam <105701288+zibonislam@users.noreply.github.com>
2023-02-15 13:33:47 +0600

    commit

--- src/main/java/com/manpower/management/repository/TraineeUpdateRepository.java
+++ src/main/java/com/manpower/management/repository/TraineeUpdateRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface TraineeUpdateRepository extends JpaRepository<TraineeUpdateEntity, Long> {
+//  Optional<TraineeUpdateEntity>
 
     Optional<TraineeUpdateEntity> findById(long id);
 }
